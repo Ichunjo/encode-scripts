4778ad96755fd4001639db83df02013db99ea1d1
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-05-27 15:55:40 +0200

    vardautomation: add placebo_shaders

---
+++ vardautomation/placebo_shaders.py
@@ -0,0 +1,6 @@
+"""Shader file paths for vs.placebo module"""
+
+FSRCNNX_56_16_4_1: str = r"C:\Users\Varde\mpv\Shaders\FSRCNNX_x2_56-16-4-1.glsl"
+FSRCNNX_16_0_4_1: str = r"C:\Users\Varde\mpv\Shaders\FSRCNNX_x2_16-0-4-1-edit"
+KRIGBILATERAL: str = r"C:\Users\Varde\mpv\Shaders\KrigBilateral.glsl"
+SSIMDOWNSCALER: str = r"C:\Users\Varde\mpv\Shaders\SSimDownscaler.glsl"
