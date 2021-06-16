db8a731a46df0012935aab9358a0f18a261b41c0
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-06-16 23:39:26 +0200

    vardautomation: add presets for chapters

--- vardautomation/vardautomation/presets.py
+++ vardautomation/vardautomation/presets.py
@@ -1,6 +1,12 @@
 """Presets"""
 
-__all__ = ['Preset', 'PresetBD', 'PresetWEB', 'PresetAAC', 'PresetOpus', 'PresetEAC3', 'PresetFLAC', 'NoPreset']
+__all__ = [
+    'Preset', 'NoPreset',
+    'PresetBD', 'PresetWEB',
+    'PresetAAC', 'PresetOpus', 'PresetEAC3', 'PresetFLAC',
+    'PresetChapOGM',
+    'PresetChapXML'
+]
 
 import os
 from typing import Callable, Optional
@@ -27,11 +33,66 @@ class Preset():  # noqa
         super().__init__()
 
 
-PresetBD = Preset(core.lsmas.LWLibavSource, '{path:s}_track_{num:s}.wav', '{path:s}_cut_track_{num:s}.wav', None, os.path.abspath('chapters/{name:s}.txt'))
-PresetWEB = Preset(core.ffms2.Source, None, None, '', '')
-PresetAAC = Preset(None, '{path:s}_track_{num:s}.aac', '{path:s}_cut_track_{num:s}.aac', '{path:s}_cut_enc_track_{num:s}.m4a', None)
-PresetOpus = Preset(None, '{path:s}_track_{num:s}.opus', '{path:s}_cut_track_{num:s}.opus', '{path:s}_cut_enc_track_{num:s}.opus', None)
-PresetEAC3 = Preset(None, '{path:s}_track_{num:s}.eac3', '{path:s}_cut_track_{num:s}.eac3', '{path:s}_cut_enc_track_{num:s}.eac3', None)
-PresetFLAC = Preset(None, '{path:s}_track_{num:s}.flac', '{path:s}_cut_track_{num:s}.flac', '{path:s}_cut_enc_track_{num:s}.flac', None)
-
-NoPreset = Preset(lvsfunc.misc.source, '', '', '', '')
+PresetBD = Preset(
+    idx=core.lsmas.LWLibavSource,
+    a_src='{path:s}_track_{num:s}.wav',
+    a_src_cut='{path:s}_cut_track_{num:s}.wav',
+    a_enc_cut=None,
+    chapter=None
+)
+PresetWEB = Preset(
+    idx=core.ffms2.Source,
+    a_src=None,
+    a_src_cut=None,
+    a_enc_cut='',
+    chapter=''
+)
+PresetAAC = Preset(
+    idx=None,
+    a_src='{path:s}_track_{num:s}.aac',
+    a_src_cut='{path:s}_cut_track_{num:s}.aac',
+    a_enc_cut='{path:s}_cut_enc_track_{num:s}.m4a',
+    chapter=None
+)
+PresetOpus = Preset(
+    idx=None,
+    a_src='{path:s}_track_{num:s}.opus',
+    a_src_cut='{path:s}_cut_track_{num:s}.opus',
+    a_enc_cut='{path:s}_cut_enc_track_{num:s}.opus',
+    chapter=None
+)
+PresetEAC3 = Preset(
+    idx=None,
+    a_src='{path:s}_track_{num:s}.eac3',
+    a_src_cut='{path:s}_cut_track_{num:s}.eac3',
+    a_enc_cut='{path:s}_cut_enc_track_{num:s}.eac3',
+    chapter=None
+)
+PresetFLAC = Preset(
+    idx=None,
+    a_src='{path:s}_track_{num:s}.flac',
+    a_src_cut='{path:s}_cut_track_{num:s}.flac',
+    a_enc_cut='{path:s}_cut_enc_track_{num:s}.flac',
+    chapter=None
+)
+NoPreset = Preset(
+    idx=lvsfunc.misc.source,
+    a_src='',
+    a_src_cut='',
+    a_enc_cut='',
+    chapter=''
+)
+PresetChapOGM = Preset(
+    idx=None,
+    a_src=None,
+    a_src_cut=None,
+    a_enc_cut=None,
+    chapter=os.path.abspath('chapters/{name:s}.txt')
+)
+PresetChapXML = Preset(
+    idx=None,
+    a_src=None,
+    a_src_cut=None,
+    a_enc_cut=None,
+    chapter=os.path.abspath('chapters/{name:s}.xml')
+)
