d5cbbbd47509d8b3fea48c7790e802ba4e37ce91
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-06-20 18:55:12 +0200

    vardautomation: add some method to VPath

--- vardautomation/vardautomation/vpathlib.py
+++ vardautomation/vardautomation/vpathlib.py
@@ -13,6 +13,9 @@ AnyPath = Union[PathLike, str]
 class VPath(Path):
     _flavour = type(Path())._flavour  # type: ignore
 
+    def __format__(self, format_spec: str) -> str:
+        return str(self)
+
     def format(self, *args, **kwargs):
         """
             vpath.format(*args, **kwargs) -> VPath
@@ -21,3 +24,10 @@ class VPath(Path):
             The substitutions are identified by braces ('{' and '}')
         """
         return VPath(str(self).format(*args, **kwargs))
+
+    def to_str(self) -> str:
+        """
+            Return the string representation of the path, suitable for
+            passing to system calls.
+        """
+        return str(self)
