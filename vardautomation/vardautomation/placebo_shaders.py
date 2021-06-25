8803a5375d026611de3e40213099f7207342742b
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-06-25 03:47:35 +0200

    vardautomation: become Enum

--- vardautomation/vardautomation/placebo_shaders.py
+++ vardautomation/vardautomation/placebo_shaders.py
@@ -1,6 +1,12 @@
 """Shader file paths for vs.placebo module"""
 
-FSRCNNX_56_16_4_1: str = r"C:\Users\Varde\mpv\Shaders\FSRCNNX_x2_56-16-4-1.glsl"
-FSRCNNX_16_0_4_1: str = r"C:\Users\Varde\mpv\Shaders\FSRCNNX_x2_16-0-4-1-edit.glsl"
-KRIGBILATERAL: str = r"C:\Users\Varde\mpv\Shaders\KrigBilateral.glsl"
-SSIMDOWNSCALER: str = r"C:\Users\Varde\mpv\Shaders\SSimDownscaler.glsl"
+from enum import Enum
+
+__all__ = ['Shader']
+
+
+class Shader(Enum):
+    FSRCNNX_56_16_4_1 = r"C:\Users\Varde\mpv\Shaders\FSRCNNX_x2_56-16-4-1.glsl"
+    FSRCNNX_16_0_4_1 = r"C:\Users\Varde\mpv\Shaders\FSRCNNX_x2_16-0-4-1-edit.glsl"
+    KRIGBILATERAL = r"C:\Users\Varde\mpv\Shaders\KrigBilateral.glsl"
+    SSIMDOWNSCALER = r"C:\Users\Varde\mpv\Shaders\SSimDownscaler.glsl"
