1c47558dfa24de478aa8cd14526bb15ffcb88bad
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-08-10 17:11:56 +0200

    upload Magia Record S202

--- Other groups/YameteTomete/Magia Record S2 [WEB]/magia_common/config.py
+++ Other groups/YameteTomete/Magia Record S2 [WEB]/magia_common/config.py
@@ -9,41 +9,39 @@ from vardefunc.types import Range
 core = vs.core
 
 
-
 class Encoding:
-    runner: SelfRunner
+    v_encoder: X265Encoder
 
     def __init__(self, file: FileInfo, clip: vs.VideoNode) -> None:
         self.file = file
         self.clip = clip
-        assert self.file.a_src
-
-
-        self.v_encoder = X265Encoder('magia_common/x265_settings')
-        self.a_extracters = BasicTool('mkvextract', [self.file.path.to_str(), 'tracks', f'1:{self.file.a_src.set_track(1)}'])
-        self.a_cutters = EztrimCutter(self.file, track=1)
 
     def run(self) -> None:
+        assert self.file.a_src
         assert self.file.a_src_cut
+
+        self.v_encoder = X265Encoder('magia_common/x265_settings')
+        a_extracters = BasicTool('mkvextract', [self.file.path.to_str(), 'tracks', f'1:{self.file.a_src.set_track(1)}'])
+        a_cutters = EztrimCutter(self.file, track=1)
 
         muxer = Mux(
             self.file,
             streams=(
                 VideoStream(self.file.name_clip_output, 'HEVC WEBRip by Vardë@Raws-Maji', JAPANESE),
-                [AudioStream(self.file.a_src_cut.set_track(1), 'EAC3 2.0', JAPANESE)],
+                AudioStream(self.file.a_src_cut.set_track(1), 'EAC3 2.0', JAPANESE),
                 None
-            )
+            ), merge_args={'--quiet': None, '--ui-language': 'en'}
         )
         # muxer = Mux(self.file)
 
         config = RunnerConfig(
             self.v_encoder, None,
-            self.a_extracters, self.a_cutters, None,
+            a_extracters, a_cutters, None,
             muxer
         )
 
-        self.runner = SelfRunner(self.clip, self.file, config)
-        self.runner.run()
+        runner = SelfRunner(self.clip, self.file, config)
+        runner.run()
 
     def do_patch(self, ranges: Union[Range, List[Range]]) -> None:
         p = Patch(self.v_encoder, self.clip, self.file, ranges)
